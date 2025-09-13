5c6daa57db35e221773bcd8cfe466f334531e7d6
Shantanupokale <shantanupokale51@gmail.com>
2025-09-13 12:19:03 +0530

    added random data

--- react1/src/ExpenseData.js
+++ react1/src/ExpenseData.js
@@ -1,5 +1,26 @@
 export default [
     {
-        title:""
-    }
+        id:crypto.randomUUID(),
+        title: 'Milk',
+        category: 'Grocery',
+        amount:40
+    },
+    {
+        id:crypto.randomUUID(),
+        title: 'Shirt',
+        category: 'Clothes',
+        amount:600
+    },
+    {
+        id:crypto.randomUUID(),
+        title: 'Vegetables',
+        category: 'Grocery',
+        amount:100
+    },
+    {
+        id:crypto.randomUUID(),
+        title: 'Electricity Bill',
+        category: 'Bills',
+        amount:1100
+    },
 ]
