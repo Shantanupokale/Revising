26fb292558b55234fbae4c4a0e3e2d6068764750
Shantanupokale <shantanupokale51@gmail.com>
2025-10-04 00:15:16 +0530

    some extra code and store logic

--- zustand/src/store/cartStore.ts
+++ zustand/src/store/cartStore.ts
@@ -1,13 +1,34 @@
+import type { cartProduct } from "@/types/cartProduct";
 import type { Product } from "@/types/product";
 import { StateCreator } from "zustand";
 
 type CartState = {
-    products: (Product & { qnty: number })[];
+    products: cartProduct[];
     total: number;
 
 };
 
 type CartActions = {
-    
+    addProduct: (product: Product) => void;
+    removeproduct: (productId: string) => void;
+    incQty: (productId: string) => void;
+    decQty: (productId: string) => void;
+    getProductbyId: (productId: string) => cartProduct | undefined
+    setTotal: (total: number) => void;
+    reset: () => void;
+};
+
+export type CartSlice = CartState & CartActions
+
+const initialState: CartState = {
+    products: [],
+    total: 0,
 }
 
+
+
+export const cerateCartSlice: StateCreator<CartSlice, [], [], CartSlice> = (set) => ({
+    ...initialState, incQty(product) => set((state) =>
+    const
+    )
+})
---
+++ zustand/src/types/cartProduct.ts
@@ -0,0 +1,3 @@
+import type { Product } from "./product";
+
+export type cartProduct = Product & { qty: number };
